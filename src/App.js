47bc197c5c0be807947eed12ddcbd8307e187714
xorsx <oliviarswann@gmail.com>
2025-01-07 16:24:25 -0500

    added weather project code from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import "./App.css";
-import Weather from "./Weather.js";
+import Weather from "./Weather";
+import "./index.css";
+import "./Weather.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Weather city="Toronto" />
+      <header>
+        <form className="search-form" id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            required
+            className="search-form-input"
+            id="search-input"
+          />
+          <input type="submit" value="Search" className="search-form-button" />
+        </form>
+      </header>
+      <Weather />
+      <footer>
+        Built by
+        <a href="https://github.com/xorsx" target="_blank">
+          Olivia Swann
+        </a>
+        open-sourced on
+        <a href="https://github.com/xorsx/weather-app-project" target="_blank">
+          Github
+        </a>
+        and hosted on
+        <a
+          href="https://coding-project-weather-app.netlify.app/"
+          target="_blank"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,43 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )} degrees celcius`
-    );
-  }
+export default function Weather() {
+  let weatherData = {
+    city: "Denver",
+    humidity: 20,
+    wind: 15,
+    temperature: 45,
+    forecast: "Snowy",
+    datetime: "Last updated: Wednesday 5:30p.m.",
+    icon: "",
+  };
+  return (
+    <main>
+      <div className="weather-app-data">
+        <div>
+          <h1 className="weather-app-city">{weatherData.city}</h1>
+          <p className="weather-app-details">
+            <span>{weatherData.datetime}</span>
+            <br />
+            Humidity: <strong>{weatherData.humidity}%</strong> Wind:
+            <strong>{weatherData.wind}mph</strong>
+          </p>
+        </div>
+        <div className="weather-app-temperature-container">
+          <div>
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUlJREFUaN7t2cGxgjAQgGFKsIRXAiVQgiVQgiWkBEqwFErg+t8ogQ7iJTiKPscNuzHRZGYvzKzul1mWMDTe+6bkKLr4CsgSIFlAC7hNtI3hUgGEwifA/xOTFWQ3AOhfFL6NPisA0AmKX6PNCTBFAMYsAKHvfWScgXlzbQYG4JAK4HYAXsUCHFMABiPAGsfSAcs77RQFAA6hj71xDOqA0PtLguI9MKsBwq5PiQq/hiYgefFqAMORaQ8IrbN8CvDkUNhLAX0mxd89ySUAlyHgOmJLBnjgr3TAqXSAewfQFg0Iz4Gx2Bba8eqYx02c+4o9Tjugi/lD7dxYgAdcZBGquSLAs5Eq3D31XCmgu/mxUbKTVrm/1UJfM4UqoAIMAevEiLxJxbkWACeZ8XtztT8xPRx3rXO1Ae7m6C0688Tmfn8L1e/EFVABFZA0LkcjEH9Eoxa+AAAAAElFTkSuQmCC"
+              alt="weather icon"
+            />
+          </div>
+          <div className="weather-app-temperature-value">
+            {weatherData.temperature}
+          </div>
+          <div className="weather-app-temperature-unit">°F</div>
+        </div>
+      </div>
 
-  let apiKey = "c6f8ef4575250284954db9f4dfa7a996&units=metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from weather</h2>;
+      <div className="weather-forecast">{weatherData.forecast}</div>
+    </main>
+  );
 }
